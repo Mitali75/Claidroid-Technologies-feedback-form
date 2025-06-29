57a8d2a5a3b80d6f13f816cf1bce6db38a090d26
Mitali75 <mitalirandive.0075@gmail.com>
2025-06-30 00:05:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def logout():
 
 # ---------------- Run ----------------
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
